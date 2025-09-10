ad6fd6f7278933c423503f5ec16a129bebddb2f2
ghodekarpratik200-arch <ghodekarpratik200@gmail.com>
2025-09-11 00:39:10 +0530

    product carousel initinal commint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HeroSection from '../compoents/HeroSection';
 import ServicesSection from '../compoents/ServicesSection';
 import ClientsSection from '../compoents/ClientsSection';
+import ServicesCarousel from '../compoents/ServicesCarousel/ServicesCarousel';
 
 
 function Home() {
   return (
     <>
       <HeroSection/>
+      <ServicesCarousel/>
       <ServicesSection/>
       <ClientsSection/>
     </>
